c09f8439bf98d9d246408a9a8c209def0530782a
Romita Sharma <romitasharma@Romitas-MacBook-Air.local>
2025-02-04 20:42:32 +0530

    goods

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ const Home = () => {
           const service  = await window.getDigitalGoodsService('https://play.google.com/billing');
           if(service){
         alert('service');
-            const goodsDetails = service.getDetails(['coins_20', 'coin_50']).then(response => {
+            const goodsDetails = service.getDetails(['coin_20', 'coin_50']).then(response => {
               alert(JSON.stringify(response));
               setGoodsDetails(goodsDetails); 
             }).catch(error => {
