808a146c24da088e7712f3ca8fcc61b71cb61c4d
Romita Sharma <romitasharma@Romitas-MacBook-Air.local>
2025-02-11 20:23:35 +0530

    commit

--- public/sw.js
+++ public/sw.js
@@ -46,6 +46,14 @@ if (!self.define) {
     );
   };
 
+  self.addEventListener("alarm", (event) => {
+    self.registration.showNotification("Daily Reminder", {
+        body: "Don't forget to read today 📖",
+        icon: "/icon-512x512.png",
+        // badge: "/badge.png"
+    });
+});
+
   self.define = (depsNames, factory) => {
     const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
     if (registry[uri]) {
