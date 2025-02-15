89b04a63a361106409868654ec99e67198614a2d
Romita Sharma <romitasharma@Romitas-MacBook-Air.local>
2025-02-15 14:24:45 +0530

    design

--- src/firebase/googleAnalytics.js
+++ src/firebase/googleAnalytics.js
@@ -2,6 +2,6 @@ import { logEvent } from "firebase/analytics";
 import { analytics } from "./";
 import { storage } from "@/app/utility";
 
-export const logGAEvent = (eventName, params = {userId: JSON.parse(storage.getItem('user')).email}) => {
+export const logGAEvent = (eventName, params = {userId: JSON.parse(storage.getItem('user')).email.replace('@', '[at]')}) => {
     logEvent(analytics, eventName, params);
 };
